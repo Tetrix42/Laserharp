33b354b7ddf63d6ced425fd70cb00aeb945b2ec5
Felix Passenberg <FeLUX42@gmail.com>
2016-08-05 16:10:15 +0200

    fixes in music, new play_with_blobdetection
    simpleBlobDetection replaces contours

--- vorsichtLaserHarfe/music.py
+++ vorsichtLaserHarfe/music.py
@@ -9,9 +9,9 @@ class music:
 	lowestpitch = 60	# 60 is normal c, 61 is c#, 62 is d ....
 	useFullSteps = True	# use ganzton or halbton steps, 60->62, or 60->61
 	midiout = 0;
-	instrument = 69;
+	instrument = 19;
 	
-	def __init__(self, beams, mods = 0, lowestpitch = 60, useFullSteps = True, instrument = 69):	## sets the number of beams. and builds the status array.
+	def __init__(self, beams, mods = 0, lowestpitch = 60, useFullSteps = True, instrument = 19):	## sets the number of beams. and builds the status array.
 		self.beams = beams;
 		self.active = {}
 		self.modifier = mods + 1
@@ -34,15 +34,14 @@ class music:
 			for b in range(self.beams):
 				pit = self.pitch(b, m, self.useFullSteps);
 				self.active[pit]= 0;
-				#midiout.send_message([0x90, pit, 127]) ## start the tones 
-				#midiout.send_message([0xB0, 07, 0])    ## and set their volume to 0
+				self.midiout.send_message([0xC0+b,self.intstrument])
 		
 	def pitch(self, beam, modifier = 0, full = True):
 		if full == True:	
 			return self.lowestpitch + beam*2 + modifier*2*self.beams;	## returns pitch with "ganztonschritt"
 		if full == False:
 			return self.lowestpitch + beam + modifier*self.beams;		## returns pitch with "halbtonschritt"
-		midiout.send_message([0xC0+beam,self.intstrument])
+		#self.midiout.send_message([0xC0+beam,self.intstrument])
 	
 	def play(self, beam, amp, modifier = 0):
 		pit = self.pitch(beam, modifier, self.useFullSteps)
@@ -50,11 +49,13 @@ class music:
 			if self.active[pit] == 0:
 				self.midiout.send_message([0x90+beam, pit, 127])
 				self.midiout.send_message([0xB0+beam, 07, amp]) ## here absolute volume is set.
-				self.active[pit] = amp;
+				#self.active[pit] = amp;
 			elif self.active[pit] != 0:
 				self.midiout.send_message([0xB0+beam, 07, amp]) ## here absolute volume is set.
 		else:	## amp <= 0
+			pass
 			self.midiout.send_message([0x80+beam, pit, 127])
+		self.active[pit] = amp;
 		
 	def test(self):
 		print("Timidity music test...")
